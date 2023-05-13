9b8cfefb6d2beb5627947bbb0aba0da65bf82b7f
BuildTools <unconfigured@null.spigotmc.org>
2023-05-13 10:44:20 -0500

    Updated main.py changed demo file to work with new changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ width, height = 1600, 900
 sc = pygame.display.set_mode([width, height])
 pygame.display.set_caption('3D Test')
 
-c = FPSCamera(Vector3(0, 0, 0), Vector3(0, 0, 0), 1, 0.05)
+c = FPSCamera(Vector3(-5, 0, 0), Vector3(0, 0, 0), 1, 0.05)
 o1 = Cube(Vector3(1, 1, 1), 10, [(255, 0, 0), (255, 0, 0), (0, 255, 0), (0, 255, 0), (0, 0, 255), (0, 0, 255)])
 o2 = Cube(Vector3(20, 1, 1), 5, [(255, 255, 0), (255, 255, 0), (0, 255, 255), (0, 255, 255), (255, 0, 255), (255, 0, 255)])
 scene1 = Scene([o1, o2])
@@ -14,7 +14,8 @@ scene1 = Scene([o1, o2])
 def update():
     sc.fill((255, 255, 255))
     scene1.render(sc, c)
-    c.update()
+    o1.rotate(Vector3(0.01, 0, 0.01))
+    c.update(scene1)
     pygame.display.update()
 
 
