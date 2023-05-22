14033a024e6dc0c4d978e20a6fdb3b6c012e2dcb
pok <81258018+Pokhanpat@users.noreply.github.com>
2023-05-22 14:32:58 -0500

    Update main.py
    yall idk

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from td import *
 import pygame
+pygame.init()
 
-width, height = 1600, 900
+width, height = 400, 400
 sc = pygame.display.set_mode([width, height])
 pygame.display.set_caption('3D Test')
 
-c = FPSCamera(Vector3(-5, 0, 0), Vector3(0, 0, 0), 1, 0.05)
+frame = 0
+
+c = FPSCamera(Vector3(-5, 0, 0), Vector3(0, 0, 0), 1, 0.05, aspect_ratio=width/height)
 o1 = Cube(Vector3(1, 1, 1), 10, [(255, 0, 0), (255, 0, 0), (0, 255, 0), (0, 255, 0), (0, 0, 255), (0, 0, 255)])
 o2 = Cube(Vector3(20, 1, 1), 5, [(255, 255, 0), (255, 255, 0), (0, 255, 255), (0, 255, 255), (255, 0, 255), (255, 0, 255)])
 scene1 = Scene([o1, o2])
 
 
 def update():
-    sc.fill((255, 255, 255))
+    sc.fill((0, 0, 0))
     scene1.render(sc, c)
     o1.rotate(Vector3(0.01, 0, 0.01))
+    o2.move(Vector3(sin(PI/30 * frame), 0, 0))
     c.update(scene1)
     pygame.display.update()
 
 
 clock = pygame.time.Clock()
 while True:
+    frame += 1
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
